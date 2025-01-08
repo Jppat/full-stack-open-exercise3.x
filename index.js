98c14aa5285899c8b9d435f7f2594e79b6fc6998
Patrick <jtpanizales@up.edu.ph>
2025-01-09 02:34:44 +0800

    add middleware to serve static files

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ let persons = [
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :request-body'))
 app.use(cors())
+app.use(express.static('dist'))
 
 const checkUniqueness = (body, bodyProp) => {
     const match = persons.filter(person => person[bodyProp] === body[bodyProp])
