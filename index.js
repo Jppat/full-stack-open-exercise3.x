fe76e48e6682ed8c0522ae117b2568fe9e8e3584
Patrick <jtpanizales@up.edu.ph>
2025-01-09 01:51:13 +0800

    listen to PORT defined in env variable or 3001

--- index.js
+++ index.js
@@ -118,8 +118,7 @@ app.get('/', (request, reponse) => {
     reponse.send('<h1>Welcome to phonebook backend</h1>')
 })
 
-const PORT = 3001
-app.listen(PORT,() => {
-    console.log(`Server running on port ${PORT}`)
-    
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 })
